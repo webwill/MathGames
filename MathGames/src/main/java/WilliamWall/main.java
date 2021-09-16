c753f4b85a979b956d40bcc8d9176871c0dbc646
webwill <williamjohnwall@gmail.com>
2021-09-16 22:30:30 +0100

    Adding a difficulty Feature
    Allowing the user to select the hardest of the maths

--- MathGames/src/main/java/WilliamWall/main.java
+++ MathGames/src/main/java/WilliamWall/main.java
@@ -20,13 +20,37 @@ public class main {
   public static void main(String[] args){
       
        int gameScore =0;
+       int max = 1;
+       int min = 0;
+       
+       
        Scanner scanner = new Scanner(System.in);
        
        
+       while(true){
+       System.out.println("Hoe hard do you want the cums to be? (TYPE: HARD, MEDIUM OR EASY)");
+       String difficulty = scanner.nextLine();
        
-       int max = 10;
-       int min = 1;
+       if(difficulty.equalsIgnoreCase("hard")){
+           max = 250;
+           min = 20;
+           break;
+       }else if(difficulty.equalsIgnoreCase("medium")){
+           max = 50;
+           min = 10;
+           break;
+       }else if(difficulty.equalsIgnoreCase("easy")){
+           max = 15;
+           min = 1;
+           break;
+       }else{
+           System.out.println("Please type one of the following: HARD , MEDIUM OR EASY");
+       }
+       }
+       
+       
        int range = max - min + 1;
+       
        
        for( int i =0; i< 10; i++){
             int numOne =  (int) (Math.random() * range) + min;
