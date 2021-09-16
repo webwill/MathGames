0c4db925312ac7f7f4e65eb5307774f77af58ea0
webwill <williamjohnwall@gmail.com>
2021-09-16 21:53:09 +0100

    Created Simple Game Logic
    Simple adding Game

--- MathGames/src/main/java/WilliamWall/main.java
+++ MathGames/src/main/java/WilliamWall/main.java
@@ -7,13 +7,53 @@ package WilliamWall;
 
 /**
  *
- * @author willi
+ * @author william wall
  */
-public class main {
 
-  public  static void main(String[] args){
-	System.out.println("Hello World");
-  }
+
+import java.util.Scanner;
+
+public class main {
+   
+  
     
+  public static void main(String[] args){
+      
+       int gameScore =0;
+       Scanner scanner = new Scanner(System.in);
+       
+       
+       
+       int max = 10;
+       int min = 1;
+       int range = max - min + 1;
+       
+       for( int i =0; i< 10; i++){
+            int numOne =  (int) (Math.random() * range) + min;
+            int numTwo =  (int) (Math.random() * range) + min;
+
+            System.out.println("What is " + numOne +" + " + numTwo + "?");
+            
+            String ans = scanner.nextLine();
+            
+            int result =0;
+            
+            try{
+            
+            result = Integer.parseInt(ans);
+            }catch(NumberFormatException e){
+            
+                System.err.println("Please Enter a vaild number");
+            }
+            
+            if ( (numOne + numTwo) == result) {
+               gameScore += 5;
+           }
+       }
+        System.out.println("Your Total Score is " + gameScore);
+  }
+  
+  
+  
     
 }
