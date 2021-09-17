e3cf446a6e134f71a105bdb0ee5673583e0f51ef
webwill <williamjohnwall@gmail.com>
2021-09-17 01:34:04 +0100

    refactoring the question/answering code
    Moving Question code from Main to Question class

--- MathGames/src/main/java/WilliamWall/Question.java
+++ MathGames/src/main/java/WilliamWall/Question.java
@@ -11,19 +11,60 @@ package WilliamWall;
  */
 public class Question {
     
+    private int range,max,min,numOne,numTwo,answer,operator;
+    private String question;
     
-    public Question(int difficulty){
-        
+    public Question(String difficulty){
+        switch(difficulty){
+            case "hard":
+              max = 250;
+              min = 20;
+              break;
+              
+           case "medium":
+           max = 50;
+           min = 10;
+           break;
+           
+           case "easy":
+           max = 15;
+           min = 1;
+           break;
+    }
+     range = (int)(max-min+1) + min;
+     numOne =  (int) (Math.random() * range) + min;
+     numTwo =  (int) (Math.random() * range) + min;
+     operator =(int) (Math.random()*(4-1+1)+1);
+     /*
+     switch(operator){
+         case 1:
+             addQuestion();
+         break;
+         case 2:
+             subQuestion();
+        break;            
+     }
+*/
+     addQuestion();
     }
     
-    public String getMathsQuestion(){
-        return "";
+    public String getQuestion(){
+        return question;
     }
     
+    public boolean answerQuestion(int ans){
+        return answer == ans;
+    }
     
     
     public Boolean answeredCorrently(){
         return false;
     }
+    
+    private void addQuestion(){
+     question =  numOne + " + " + numTwo + "?";
+     answer = numOne + numTwo;
+    }
+    private void subQuestion(){}
     
 }
--- MathGames/src/main/java/WilliamWall/main.java
+++ MathGames/src/main/java/WilliamWall/main.java
@@ -11,113 +11,65 @@ package WilliamWall;
  */
 
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main {
    
-  
+   
     
   public static void main(String[] args){
       
-       
-       int max = 1;
-       int min = 0;
+       ArrayList<Question> questions = new ArrayList<>(); 
       
+       String difficulty;
+       
+       //Score is used to track user's score
        Score score = Score.getInstance();
        
        Scanner scanner = new Scanner(System.in);
        
        
        while(true){
-       System.out.println("Hoe hard do you want the cums to be? (TYPE: HARD, MEDIUM OR EASY)");
-       String difficulty = scanner.nextLine();
-       
-       if(difficulty.equalsIgnoreCase("hard")){
-           max = 250;
-           min = 20;
-           break;
-       }else if(difficulty.equalsIgnoreCase("medium")){
-           max = 50;
-           min = 10;
-           break;
-       }else if(difficulty.equalsIgnoreCase("easy")){
-           max = 15;
-           min = 1;
-           break;
-       }else{
+       System.out.println("Hoe hard do you want the sums to be? (TYPE: HARD, MEDIUM OR EASY)");
+       difficulty = scanner.nextLine();
+        
+           //Validate the user input
+           if (difficulty.equalsIgnoreCase("hard") || difficulty.equalsIgnoreCase("medium") || difficulty.equalsIgnoreCase("easy")){ 
+               break;
+           } 
+           
            System.out.println("Please type one of the following: HARD , MEDIUM OR EASY");
+       
        }
-       }
        
-       int operator;
-       
-       
-       
-       int range = max - min + 1;
-       
-       for( int i =0; i< 10; i++){
-            int numOne =  (int) (Math.random() * range) + min;
-            int numTwo =  (int) (Math.random() * range) + min;
+       //The Game itself
+       for(int i =0; i< 10; i++){
+            //Create a new Question based on the difficulty selected
+            Question q = new Question(difficulty);
             
-            operator  =(int) (Math.random()*(4-1+1)+1);
-            
-            switch (operator){
-            case 1:
-            System.out.println("What is " + numOne + " + " + numTwo + "?");
-            break;
-            case 2:
-            System.out.println("What is " + numOne + " - " + numTwo + "?");
-            break;
-            case 3:
-            System.out.println("What is " + numOne + " % " + numTwo + "?");
-            break;
-            case 4:
-            System.out.println("What is " + numOne + " * " + numTwo + "?");
-            break;
-            }
-            
-            int result =0;
-            
+            //loop until the user enters a number
             while(true){
-            String ans = scanner.nextLine();
+            System.out.println(q.getQuestion());
             
             try{
+            int answer = Integer.parseInt(scanner.nextLine());
             
-            result = Integer.parseInt(ans);
+            //check the user's answer and add to the score if correct!
+            if(q.answerQuestion(answer)){
+                score.addScore();
+            } 
             break;
             }catch(NumberFormatException e){
                 System.out.println("Please Enter a vaild number");
             }
-            }
-            
-            switch (operator){
-            case 1:
-                if ( (numOne + numTwo) == result) {
-                   score.addScore();
-                }
-            break;
-            case 2:
-            if ( (numOne - numTwo) == result) {
-                   score.addScore();
-                }
-            break;
-            case 3:
-            if ( (numOne % numTwo) == result) {
-                   score.addScore();
-                }
-            break;
-            case 4:
-            if ( (numOne * numTwo) == result) {
-                   score.addScore();
-                }
-            break;
-            }
+         }
+            //add the question to the arraylist!(saved for the reporting)
+            questions.add(q);
        }
-        System.out.println("Your Total Score is " + score.getScore());
-        System.exit(0);
+       
+       //Tell the user their score and end the game"!
+       System.out.println("Your score is " + score.getScore());
+       System.exit(0);
   }
-  
-  
-  
-    
 }
